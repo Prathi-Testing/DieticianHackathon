a04b5fef10ff1d3670065f05b22a59c92000dee5
Sumathi Selvaraj <sumi.selvaraj86@gmail.com>
2022-06-12 17:36:04 -0600

    added feature pages step def generic file - sumathi

--- src/test/java/com/base/PageObjectManager.java
+++ src/test/java/com/base/PageObjectManager.java
@@ -2,14 +2,24 @@ package com.base;
 
 import org.openqa.selenium.WebDriver;
 
+import com.pages.*;
 import com.pages.DieticianHomePage;
+import com.pages.HomePage;
 import com.pages.NewPatientPage;
+import com.pages.TeamPage;
+
+
 
 public class PageObjectManager {
 	
 	public WebDriver driver;
 	private DieticianHomePage dieticianHP;
 	private NewPatientPage newPatientPg;
+	private HomePage homePage;
+	private TeamPage teamPage;
+	private ContactusPage contactusPage;
+	private SignUpPage signupPage;
+
 	
 	public PageObjectManager(WebDriver driver)
 	{
@@ -25,6 +35,38 @@ public class PageObjectManager {
 	{
 		dieticianHP = new DieticianHomePage(driver);
 		return dieticianHP;
+	}
+	
+	public HomePage getHomePage()
+
+	{
+		homePage = new HomePage(driver);
+
+		return homePage;
+	}
+
+	public ContactusPage getContactusPage()
+
+	{
+		contactusPage = new ContactusPage(driver);
+
+		return contactusPage;
+	}
+
+	public SignUpPage getSignupPage()
+
+	{
+		signupPage = new SignUpPage(driver);
+
+		return signupPage;
+	}
+
+	public TeamPage getTeamPage()
+
+	{
+		teamPage = new TeamPage(driver);
+
+		return teamPage;
 	}
 
 }
